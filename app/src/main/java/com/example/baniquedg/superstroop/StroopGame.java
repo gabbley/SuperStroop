de88185ff918b075c43b2f9d93e55b43c979fd80
gabbley <baniquedg@bishopireton.org>
2017-02-28 15:34:44 -0500

    Added methods to create buttons

--- app/src/main/java/com/example/baniquedg/superstroop/StroopGame.java
+++ app/src/main/java/com/example/baniquedg/superstroop/StroopGame.java
@@ -1,7 +1,12 @@
 package com.example.baniquedg.superstroop;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.TextView;
 
 public class StroopGame extends AppCompatActivity {
 
@@ -9,5 +14,15 @@ public class StroopGame extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stroop_game);
+
+
+
+
+
+
+
     }
+
+
+
 }
--- app/src/main/java/com/example/baniquedg/superstroop/StroopSettings.java
+++ app/src/main/java/com/example/baniquedg/superstroop/StroopSettings.java
@@ -2,6 +2,9 @@ package com.example.baniquedg.superstroop;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
 
 public class StroopSettings extends AppCompatActivity {
 
@@ -9,5 +12,24 @@ public class StroopSettings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stroop_settings);
+
+        createEasyButtons();
+    }
+
+    //creates 4 easy buttons
+    public void createEasyButtons() {
+        Button yellow = (Button) findViewById(R.id.btnYellow);
+        Button blue = (Button) findViewById(R.id.btnBlue);
+        Button green = (Button) findViewById(R.id.btnGreen);
+        Button red = (Button) findViewById(R.id.btnYellow);
+    }
+
+    public void createHardButtons(){
+        GridLayout hardGrid = (GridLayout) findViewById(R.id.hardButtonsGrid);
+        hardGrid.setVisibility(View.VISIBLE);
+        Button purple = (Button) findViewById(R.id.btnPurple);
+        Button pink = (Button) findViewById(R.id.btnPink);
+        Button black = (Button) findViewById(R.id.btnBlack);
+        Button orange = (Button) findViewById(R.id.btnOrange);
     }
 }
