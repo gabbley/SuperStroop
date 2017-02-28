ada8968f97073ee6f52cdc1e8ee91c3e830eec36
gabbley <baniquedg@bishopireton.org>
2017-02-28 12:34:25 -0500

    Initial commit

---
+++ app/src/main/java/com/example/baniquedg/superstroop/StroopGame.java
@@ -0,0 +1,13 @@
+package com.example.baniquedg.superstroop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StroopGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stroop_game);
+    }
+}
