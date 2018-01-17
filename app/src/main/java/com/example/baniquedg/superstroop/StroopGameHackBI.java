7514583842e0bc612a1791fda561a1ea715bb6e8
gabbley <baniquedg@bishopireton.org>
2018-01-17 15:11:38 -0500

    I'm About To F Up This Game

--- app/src/main/java/com/example/baniquedg/superstroop/StroopGameHackBI.java
+++ app/src/main/java/com/example/baniquedg/superstroop/StroopGameHackBI.java
@@ -3,17 +3,13 @@ package com.example.baniquedg.superstroop;
 import android.content.Intent;
 import android.graphics.Color;
 import android.media.MediaPlayer;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.GridLayout;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
-public class StroopGame extends AppCompatActivity {
+public class StroopGameHackBI extends AppCompatActivity {
 
     //fields
     public static String stroopCompare;
